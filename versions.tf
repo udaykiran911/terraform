e6a064d524e3d60bdba6e8e2f103d181130ff27b
udaykiran911 <84428704+udaykiran911@users.noreply.github.com>
2021-05-26 08:21:51 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -14,4 +14,9 @@ terraform {
 
 provider "azurerm" {
   features {}
+
+  subscription_id = "090595a0-63b9-416c-9892-49e1a9ab4640"
+  client_id       = "a24d8f7e-e34b-4e92-8539-8be55108877c"
+  client_secret   = "cUKu1diR06~s~~N_ge4_VZvH105ujhguAx"
+  tenant_id       = "72a4fe3c-d181-41b9-8abd-50e057af482c"
 }
